c9ce0a542552044d0acb4bdfbd719394421649a7
Jamil Abbas <jamilabbas787@gmail.com>
2018-10-15 19:16:24 -0400

    Part 1 Complete

---
+++ Exercise14/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String userEntryOne;
+            Console.Write("Enter a number: ");
+            userEntryOne = Console.ReadLine();
+
+            bool firstNumberValidation = int.TryParse(userEntryOne, out int firstNumber);
+
+            for (int i = 1; i <= firstNumber; i++)
+            {
+                int iSquared = i * i;
+                Console.WriteLine($"Your number is: {iSquared}" );
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
